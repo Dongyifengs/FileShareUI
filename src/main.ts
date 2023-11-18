653ce5a6771a3c62766febdc0ee4b6309c7511cc
MoYi <1545929126@qq.com>
2023-11-18 19:59:12 +0800

    重构

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import 'vant/lib/index.css';
 import './style.css'
 import App from './App.vue'
 
-const app = createApp(App)
-
-app.use(ElementPlus)
-app.mount('#app')
+createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {readFileSync} from "fs"
-import Components from 'unplugin-vue-components/vite';
-import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    https: {
-      key: readFileSync("./keys/agent2-key.pem"),
-      cert: readFileSync("./keys/agent2-cert.pem"),
-    },
-  },
-  plugins: [vue(), Components({resolvers: [VantResolver()]})],
+  plugins: [vue()],
 })
